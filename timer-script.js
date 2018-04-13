3a998483938aa613bf9f4feab1dc482768cbda7b
Jase Languasco <jaselanguasco@gmail.com>
2018-04-12 22:51:01 -0400

    still working on mobile keyboard

--- timer-script.js
+++ timer-script.js
@@ -125,7 +125,6 @@ let audio = new Audio('times-up.mp3');
 const initTimer = () => {
 	if (clockRunning) return;
 	startBlinkingCursor();
-	secondOne.trigger('focus');
 	const keyCodes = { 48: 0, 49: 1, 50: 2, 51: 3, 52: 4, 53: 5, 54: 6, 55: 7, 56: 8, 57: 9 };
 
 	document.onkeyup = (key) => {
@@ -400,4 +399,8 @@ startButton.onclick = startTimer;
 
 // initilize the clock and add keyup event
 timeSection.onclick = initTimer;
+
+timeSection.onclick = () => {
+	secondOne.focus();
+}
 
